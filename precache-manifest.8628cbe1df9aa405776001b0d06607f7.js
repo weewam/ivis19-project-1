8f6168be251d8e40f820e1d6291b6ecd3b596948
Ibz <ibrahim.asfadai@gmail.com>
2019-01-28 08:25:09 +0100

    Updates

---
+++ precache-manifest.8628cbe1df9aa405776001b0d06607f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be68f462b9246e33f09",
+    "url": "/ivis19-project-1/static/css/main.33d30cba.chunk.css"
+  },
+  {
+    "revision": "2be68f462b9246e33f09",
+    "url": "/ivis19-project-1/static/js/main.2be68f46.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/ivis19-project-1/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "59dfa2132c1af07f6f1c",
+    "url": "/ivis19-project-1/static/js/runtime~main.59dfa213.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ivis19-project-1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c287c3f7bebeca2c4b727602d6e98990",
+    "url": "/ivis19-project-1/index.html"
+  }
+];
